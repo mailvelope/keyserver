2ab6333a15b58cc8925c205217785b87e3dedbec
Tankred Hase <mail@tankredhase.de>
2016-06-07 14:44:54 +0200

    Add example config/development.js

---
+++ config/development.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+  mongo: {
+    uri: '127.0.0.1:27017/keyserver-test',
+    user: 'keyserver-user',
+    pass: 'trfepCpjhVrqgpXFWsEF'
+  },
+
+  email: {
+    host: 'smtp.gmail.com',
+    port: 465,
+    tls: true,
+    starttls: true,
+    pgp: true,
+    auth: {
+      user: 'user@gmail.com',
+      pass: 'password'
+    },
+    sender: {
+      name: 'OpenPGP Key Server',
+      email: 'user@gmail.com'
+    }
+  }
+
+};
