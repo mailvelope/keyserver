3df1f3ad5db460c809f846998d273409c80b577f
Tankred Hase <mail@tankredhase.de>
2017-01-20 18:51:35 +0000

    Fix jscs config

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,9 +13,7 @@ module.exports = function(grunt) {
     jscs: {
       src: ['*.js', 'src/**/*.js', 'test/**/*.js'],
       options: {
-        config: ".jscsrc",
-        esnext: true, // If you use ES6 http://jscs.info/overview.html#esnext
-        verbose: true, // If you need output with rule names http://jscs.info/overview.html#verbose
+        config: ".jscsrc"
       }
     },
 
